2507ca4ba5edf1ffe2139f15a68df743dc4d42c8
rohini1504 <44237192+rohini1504@users.noreply.github.com>
2026-04-03 22:35:57 +0530

    Create tetsprod2.py

---
+++ tetsprod2.py
@@ -0,0 +1,11 @@
+# auth.py
+
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Logged in")  # debug print
+        return True
+    return False
+
+def execute_query(query):
+    import os
+    os.system(query)   # ⚠️ command injection

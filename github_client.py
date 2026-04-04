483f6e0da8de5d1d31fbaa5f43faf773d5003d63
rohini1504 <44237192+rohini1504@users.noreply.github.com>
2026-04-04 19:21:03 +0530

    Update github_client.py

--- github_client.py
+++ github_client.py
@@ -1,12 +1,17 @@
 import os
 from github import Github
 
+COMMENT_MARKER = "<!-- PR_REVIEW_BOT_COMMENT -->"
+
+
 def get_repo():
     g = Github(os.getenv("GITHUB_TOKEN"))
     return g.get_repo(os.getenv("REPO_NAME"))
 
+
 def get_pr(pr_number):
     return get_repo().get_pull(pr_number)
+
 
 def get_pr_diff(pr):
     files = pr.get_files()
@@ -18,5 +23,18 @@ def get_pr_diff(pr):
 
     return diff if diff else "No meaningful code changes detected"
 
-def post_comment(pr, message):
-    pr.create_issue_comment(message)
+
+def upsert_comment(pr, message):
+    """
+    Update existing bot comment OR create new one
+    """
+
+    comments = pr.get_issue_comments()
+
+    for comment in comments:
+        if COMMENT_MARKER in comment.body:
+            comment.edit(COMMENT_MARKER + "\n" + message)
+            return
+
+    # if not found → create new
+    pr.create_issue_comment(COMMENT_MARKER + "\n" + message)
